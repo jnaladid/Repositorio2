c80132004b1ca706e581487b2214e24e54330250
Joan <Joan@DESKTOP-MSRTH4Q>
2019-11-25 09:22:00 +0100

    Subida de tamaño

---
+++ Ej02.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $var1 = 1;
+        $var2 = 2;
+        $var3 = 3;
+
+        echo $var1 . $var2 . $var3
+        ?>
+    </body>
+</html>
